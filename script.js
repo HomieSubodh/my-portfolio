ee2e5c67606b59a175c55323f2ac1cc0ceb55299
HomieSubodh <golang602@gmail.com>
2022-01-04 17:27:53 +0600

    js added

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+const resumeButton = document.querySelector("#download_form");
+function btnReaction() {
+    alert('No Resume Yet. Sorry!!');
+}
+resumeButton.addEventListener("click", btnReaction);
